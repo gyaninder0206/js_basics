05fdf74fd13e74e43e3dfa5bad8d84e203670c7e
gyaninder0206 <gyanindermahotra@gmail.com>
2025-06-08 19:57:38 +0000

    variable_commit

---
+++ basics/variables.js
@@ -0,0 +1,18 @@
+const id= "12345"
+let Name= "rahul"
+age= "14"
+console.table([id,Name])
+// id="1234" not allowed cuz id is constant
+console.log(id)
+Name= "Gyaninder"
+age= "20"
+console.table([id,Name])
+/*
+we prefer not to use var because of issue in block scope and functional scope
+
+*/
+let something
+console.log(something)
+
+
+
