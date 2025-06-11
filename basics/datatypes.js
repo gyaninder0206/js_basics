156c3115f90d801adf8d632b06d7b273fb3bf831
gyaninder0206 <gyanindermahotra@gmail.com>
2025-06-11 20:37:02 +0000

    final datatype summary

--- basics/datatypes.js
+++ basics/datatypes.js
@@ -11,7 +11,7 @@ console.log("Hitesh")
 
 let name = "hitesh"
 let age = 18
-let isLoggedIn = false
+//let isLoggedIn = false
 let state;
 
 // number => 2 to power 53
@@ -27,3 +27,42 @@ let state;
 
 console.log(typeof undefined); // undefined
 console.log(typeof null); // object
+
+//  Primitive
+
+//  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+// const bigNumber = 3456543576654356754n
+
+
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"];
+let myObj = {
+    name: "hitesh",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+//console.log(myFunction);
+myFunction()
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
